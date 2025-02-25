1021a0b241caca9670ab42b379693fe09411d720
romanh <roman.v.helevachuk@ukd.edu.ua>
2025-02-25 12:54:42 +0200

    my 3pr

--- first_task.py
+++ first_task.py
@@ -1,1 +1,1 @@
-print("Hi, my name is Smilianets Volodymyr Mykylayovych and I’m studying software development in college")
+print("Hi, my name is Smilianets Volodymyr Mykylayovychgi and I’m studying software development in college")
