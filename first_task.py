6d613d5a5274be7f8bde7805ba9d3d0ec53b4617
classuser154 <classuser154@local.iful.edu.ua>
2025-02-25 13:08:23 +0200

    add my part of task

--- first_task.py
+++ first_task.py
@@ -1,1 +1,2 @@
 print("Hi, my name is Smilianets Volodymyr Mykylayovychgi and I’m studying software development in college")
+print("Hi, my name is Ondrzhei Pishtiek and I’m studying software development in college")
