b38d5c8bf27e7c0d5c2573059b65c036a0751e24
romanh <roman.v.helevachuk@ukd.edu.ua>
2025-02-25 13:13:16 +0200

    task complated

--- first_task.py
+++ first_task.py
@@ -1,2 +1,3 @@
 print("Hi, my name is Smilianets Volodymyr Mykylayovychgi and I’m studying software development in college")
 print("Hi, my name is Ondrzhei Pishtiek and I’m studying software development in college")
+print("Task completed")
